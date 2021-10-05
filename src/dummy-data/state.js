30c0c008dd5797c282681ca977058c1c9dd3d7c3
IvanGonzalez05 <igonzalezhansen@gmail.com>
2021-10-05 17:34:36 -0400

    Adds some dummy data as initial state to render something when the app starts

---
+++ src/dummy-data/state.js
@@ -0,0 +1,23 @@
+const state = {
+  series: [
+    {
+      id: 1,
+      name: "One Piece",
+      author: "Eiichiro Oda",
+      releaseDate: "1999",
+    },
+    {
+      id: 2,
+      name: "The Black List",
+      author: "Jon Bokenkamp",
+      releaseDate: "2013",
+    },
+    {
+      id: 3,
+      name: "Silicon Valley",
+      author: "Mike Judge",
+      releaseDate: "2014",
+    },
+  ],
+};
+export default state;
