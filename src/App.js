41937c718fb7062808e0269338bb3e993f5f31ed
IvanGonzalez05 <igonzalezhansen@gmail.com>
2021-10-05 17:35:20 -0400

    Modifies the App component created by create-react-app so that it fits this project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Index from "./components/Index";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SerieForm from "./components/SerieForm";
+import { SeriesProvider } from "./context/SeriesState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SeriesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact={true}>
+            <Index />
+          </Route>
+          <Route path="/add" component={SerieForm} />
+          <Route path="/edit/:id" component={SerieForm} />
+        </Switch>
+      </BrowserRouter>
+    </SeriesProvider>
   );
 }
 
